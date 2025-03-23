663483fcfce8941d1d084250681fe81d5d5fe53c
Volodymyr Yakovliev <ipocemonius@gmail.com>
2025-03-23 22:24:31 +0200

    menu commit

--- #dist/js/script.js
+++ #dist/js/script.js
@@ -12,5 +12,28 @@ $(function() {
         autoplayTimeout: 3000,
         autoplaySpeed: 3000,
     });
+
+    $('.header__menu .burger__link').on('click', function(){
+        $('.menu').css('display', 'flex');
+        $('body').addClass('no_scroll');
+    });
+
+    $('.menu__close').on('click', function(){
+        $('.menu').css('display', 'none');
+        $('body').removeClass('no_scroll');
+    });
+
+    $('.menu__link').on('click', function(){
+        $('.menu').css('display', 'none');
+        $('body').removeClass('no_scroll');
+    });
+
+    $("body").on('click', '[href*="#"]', function(e){
+        $.scrollTo($(this.hash), 500);
+    });
+
+    
+
+    
     
 });
